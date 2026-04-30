5f3de09daabca039c9ea36a8c3092d1452a3c3b2
JetProc <soonjae8297@gmail.com>
2026-05-01 03:10:58 +0900

    feat: 카드 번호·유효기간 표시에 고정 슬롯 스타일 적용
    Co-authored-by: jebiyeon02 <jebiyeon02@users.noreply.github.com>
    Co-authored-by: GamjaIsMine02 <GamjaIsMine02@users.noreply.github.com>

--- src/feature/CardRegister/components/CardPreviewSection/CardExpiryDateDisplay/CardExpiryDateDisplay.tsx
+++ src/feature/CardRegister/components/CardPreviewSection/CardExpiryDateDisplay/CardExpiryDateDisplay.tsx
@@ -1,15 +1,37 @@
+import styled from 'styled-components';
+
 const CardExpiryDateDisplay = ({expiryMonth, expiryYear}: {expiryMonth: string; expiryYear: string}) => {
   let delimiter = '';
   if (expiryMonth.length === 2 || expiryYear.length === 2) {
     delimiter = '/';
   }
   return (
-    <div>
-      {expiryMonth}
-      {delimiter}
-      {expiryYear}
-    </div>
+    <Container>
+      <Chunk>{expiryMonth}</Chunk>
+      <Delimiter>{delimiter}</Delimiter>
+      <Chunk>{expiryYear}</Chunk>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  gap: 2px;
+  height: 25px;
+  margin-left: 3px;
+  font-family: 'Inter', sans-serif;
+`;
+
+const Chunk = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Delimiter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default CardExpiryDateDisplay;
--- src/feature/CardRegister/components/CardPreviewSection/CardNumberDisplay/CardNumberDisplay.tsx
+++ src/feature/CardRegister/components/CardPreviewSection/CardNumberDisplay/CardNumberDisplay.tsx
@@ -1,14 +1,37 @@
+import styled from 'styled-components';
+
 const CardNumberDisplay = ({cardNumbers}: {cardNumbers: string[]}) => {
   return (
-    <div>
+    <Container>
       {cardNumbers.map((chunk, index) => {
         if (index >= 2) {
-          return <div>{'·'.repeat(chunk.length)}</div>;
+          return <MaskedChunk key={index}>{'·'.repeat(chunk.length)}</MaskedChunk>;
         }
-        return <div>{chunk}</div>;
+        return <Chunk key={index}>{chunk}</Chunk>;
       })}
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  height: 25px;
+  font-family: 'Inter', sans-serif;
+`;
+
+const Chunk = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 38px;
+  letter-spacing: 2px;
+`;
+
+const MaskedChunk = styled(Chunk)`
+  font-size: 28px;
+  letter-spacing: 1px;
+`;
 
 export default CardNumberDisplay;
