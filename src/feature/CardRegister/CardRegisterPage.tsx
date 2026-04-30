66a2ca964e24b18d72411f5ed7567ec558619956
JetProc <soonjae8297@gmail.com>
2026-05-01 03:51:48 +0900

    feat: CardRegisterPage 구조 및 상태 관리 개선
    Co-authored-by: GGGGGGGGGGGGGGG_J <GamjaIsMine02@users.noreply.github.com>
    Co-authored-by: JeongYeonJun <jebiyeon02@users.noreply.github.com>

--- src/feature/CardRegister/CardRegisterPage.tsx
+++ src/feature/CardRegister/CardRegisterPage.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import CardPreviewSection from './components/CardPreviewSection/CardPreviewSection';
 import InfoInputSection from './components/InfoInputSection/InfoInputSection';
+import styled from 'styled-components';
 
 const CardRegisterPage = () => {
   const [cardNumbers, setCardNumbers] = useState(['', '', '', '']);
@@ -20,11 +21,26 @@ const CardRegisterPage = () => {
   };
 
   return (
-    <div>
-      <CardPreviewSection cardInfo={cardInfo} />
-      <InfoInputSection cardInfo={cardInfo} cardInfoHandlers={cardInfoHandlers} />
-    </div>
+    <Wrapper>
+      <Container>
+        <CardPreviewSection cardInfo={cardInfo} />
+        <InfoInputSection cardInfo={cardInfo} cardInfoHandlers={cardInfoHandlers} />
+      </Container>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const Container = styled.div`
+  max-width: 376px;
+  max-height: 700px;
+`;
 
 export default CardRegisterPage;
