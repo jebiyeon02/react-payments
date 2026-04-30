c5ee45083fc0dc58be89120cf237ddc561a1a59a
JetProc <soonjae8297@gmail.com>
2026-05-01 03:10:54 +0900

    refactor: 카드 프리뷰를 헤더/바디 구조로 재구성
    Co-authored-by: jebiyeon02 <jebiyeon02@users.noreply.github.com>
    Co-authored-by: GamjaIsMine02 <GamjaIsMine02@users.noreply.github.com>

--- src/feature/CardRegister/components/CardPreviewSection/CardPreviewContainer/CardPreviewContainer.tsx
+++ src/feature/CardRegister/components/CardPreviewSection/CardPreviewContainer/CardPreviewContainer.tsx
@@ -8,7 +8,7 @@ const CardPreviewContainer = ({cardInfo}: {cardInfo: CardInfoType}) => {
   const {cardNumbers, expiryMonth, expiryYear} = cardInfo;
 
   const getBrandName = (cardNumbers: string[]): 'visa' | 'masterCard' | null => {
-    const fullNumber = cardNumbers.join('');
+    const fullNumber = cardNumbers.map((chunk) => chunk.padEnd(4, '#')).join('');
 
     if (fullNumber.startsWith('4')) return 'visa';
 
@@ -20,30 +20,56 @@ const CardPreviewContainer = ({cardInfo}: {cardInfo: CardInfoType}) => {
 
   return (
     <Container>
-      <IcChip></IcChip>
-      <CardBrandLogo brandName={getBrandName(cardNumbers)} />
-      <CardNumberDisplay cardNumbers={cardNumbers} />
-      <CardExpiryDateDisplay expiryMonth={expiryMonth} expiryYear={expiryYear} />
+      <CardHeader>
+        <IcChip />
+        <CardBrandLogo brandName={getBrandName(cardNumbers)} />
+      </CardHeader>
+      <CardBody>
+        <CardNumberDisplay cardNumbers={cardNumbers} />
+        <CardExpiryDateDisplay expiryMonth={expiryMonth} expiryYear={expiryYear} />
+      </CardBody>
     </Container>
   );
 };
 
 const Container = styled.div`
   display: flex;
-  position: relative;
+  flex-direction: column;
   width: 212px;
   height: 132px;
+  padding: 8px 10px;
+  gap: 12px;
   background-color: #333333;
   border-radius: 4px;
   box-shadow: 3px 3px 5px 0px rgba(0, 0, 0, 0.25);
 `;
 
-const IcChip = styled.div`
-  position: absolute;
-  top: 8px;
-  left: 12px;
-  width: 36px;
+const CardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+
   height: 22px;
+
+  width: 100%;
+`;
+const CardBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+
+  padding: 6px;
+
+  width: 100%;
+
+  color: white;
+  font-weight: 500;
+  font-size: 14px;
+`;
+
+const IcChip = styled.div`
+  width: 36px;
+
   background-color: #ddcd78;
   border-radius: 4px;
 `;
