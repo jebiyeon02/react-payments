125f8d553fd2b41e0e4bcfd12828111df5fc8e5c
JetProc <soonjae8297@gmail.com>
2026-04-30 20:08:46 +0900

    refactor: cvcNumbers를 단수형 cvcNumber로 리네임
    Co-authored-by: JeongYeonJun <jebiyeon02@users.noreply.github.com>
    Co-authored-by: GGGGGGGGGGGGGGG_J <GamjaIsMine02@users.noreply.github.com>

--- my-app/src/feature/CardRegister/components/InfoInputSection/CvcField/CvCField.tsx
+++ my-app/src/feature/CardRegister/components/InfoInputSection/CvcField/CvCField.tsx
@@ -1,26 +1,37 @@
 import Label from '../../../../../common/components/Label/Label';
 import Input from '../../../../../common/components/Input/Input';
+// import {useState} from 'react';
 
-const CvcField = ({cvcNumbers, setCvcNumbers}: {cvcNumbers: string; setCvcNumbers: (value: string) => void}) => {
-  const handleCvcNumbersChange = (eValue: string) => {
+const CvcField = ({
+  cvcNumber,
+  setCvcNumber,
+  // setIsError,
+}: {
+  cvcNumber: string;
+  setCvcNumber: (value: string) => void;
+  // setIsError: (value: boolean) => void;
+}) => {
+  const handleCvcNumberChange = (eValue: string) => {
     const value = eValue.trim();
 
     if (!/^\d*$/.test(value)) {
       return;
     }
 
-    setCvcNumbers(eValue);
+    setCvcNumber(eValue);
   };
+
   return (
     <div>
       <Label value='CVC' />
       <div>
         <Input
-          value={cvcNumbers}
+          value={cvcNumber}
           maxLength={3}
           inputMode='numeric'
           placeholder='123'
-          onChange={(e) => handleCvcNumbersChange(e.target.value)}
+          onChange={(e) => handleCvcNumberChange(e.target.value)}
+          // onBlur={(e) => handleCvcNumberBlur(e.target.value)}
         />
       </div>
       <span>에러 메시지</span>
